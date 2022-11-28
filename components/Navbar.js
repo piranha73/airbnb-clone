18b916cf9683f81a9aee67709e26d9f8ee4d27d6
Michele Comolli <mikicomolli@gmail.com>
2022-11-28 11:46:40 +0100

    Added css styling to navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,8 +1,9 @@
 import Link from "next/link"
+import styles from './Navbar.module.css'
 
 const Navbar = props => {
   return (
-    <div>
+    <div className={styles.navbar}>
       <Link href="/">Home</Link>
       <Link href="/flats">flats</Link>
     </div>
