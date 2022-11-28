1683c3f36c85720095e2cc1418614e702eb4920b
Michele Comolli <mikicomolli@gmail.com>
2022-11-28 11:31:58 +0100

    removed unreleated files

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,7 @@
 import '../styles/globals.css'
-import Navbar from '../components/Navbar'
 
 function MyApp({ Component, pageProps }) {
-  return (
-  <>
-    <Navbar></Navbar>
-    <Component {...pageProps} />
-  </>
-  )
+  return <Component {...pageProps} />
 }
 
 export default MyApp
